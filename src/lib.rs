368e5fe03178ba50f04046e2ac861b57a8b6c8ac
Mike Stipicevic <stipredirect@gmail.com>
2019-03-02 20:17:54 +0100

    Move code to lib.rs and add Travis

---
+++ src/lib.rs
@@ -0,0 +1,113 @@
+#[cfg(test)]
+#[macro_use]
+extern crate maplit;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_yaml;
+extern crate tempfile;
+
+use std::collections::BTreeMap;
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
+use dialoguer::Confirmation;
+
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+struct CheckListList(BTreeMap<String, CheckList>);
+
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+struct CheckList(Vec<String>);
+
+impl CheckListList {
+    fn from_file(path: &Path) -> Result<CheckListList, &'static str> {
+        let file = match File::open(&path) {
+            Err(_) => return Err("couldn't open file"),
+            Ok(file) => file,
+        };
+        CheckListList::from_reader(file)
+    }
+
+    fn from_reader<R: Read>(input: R) -> Result<CheckListList, &'static str> {
+        match serde_yaml::from_reader::<_, CheckListList>(input) {
+            Err(_) => Err("couldn't parse yaml"),
+            Ok(s) => Ok(s),
+        }
+    }
+}
+
+fn ask_question(prompt: &str) -> Result<bool, &'static str> {
+    match Confirmation::new().with_text(&prompt).interact() {
+        Err(_) => Err("error occured while prompting"),
+        Ok(answer) => Ok(answer),
+    }
+}
+
+fn ask_formatted_question(prefix: &str, prompt: &str) -> Result<bool, &'static str> {
+    ask_question(&format!("{}{}?", prefix, prompt))
+}
+
+fn question_loop(checklist: &CheckList) -> Result<bool, Box<dyn Error>> {
+    let mut seen = false;
+    for item in &checklist.0 {
+        if !seen {
+            seen = true;
+        } else {
+            println!("Great! Continuing...")
+        }
+        if !ask_formatted_question(&"Have you: ", &item)? {
+            println!("Aborting, please fix and start again.");
+            return Ok(false);
+        }
+    }
+
+    Ok(true)
+}
+
+pub fn run(path: &Path) -> Result<(), Box<dyn Error>> {
+    let checklists = CheckListList::from_file(&path)?;
+    if let Some(checklist) = checklists.0.get("committing") {
+        if question_loop(&checklist)? {
+            println!("all clear!")
+        }
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::{Seek, SeekFrom, Write};
+
+    fn write_to_tempfile(contents: &str) -> File {
+        // Write
+        let mut tmpfile: File = tempfile::tempfile().unwrap();
+        tmpfile.write_all(contents.as_bytes()).unwrap();
+
+        // Seek to start
+        tmpfile.seek(SeekFrom::Start(0)).unwrap();
+
+        tmpfile
+    }
+
+    #[test]
+    fn test_correct_yaml() {
+        let tempfile = write_to_tempfile("committing:\n- test");
+        assert_eq!(
+            CheckListList::from_reader(tempfile),
+            Ok(CheckListList(btreemap! {
+                String::from("committing") => CheckList(vec![String::from("test")]),
+            })),
+        )
+    }
+
+    #[test]
+    fn test_incorrect_yaml() {
+        let tempfile = write_to_tempfile("beep beep");
+        assert_eq!(
+            CheckListList::from_reader(tempfile),
+            Err("couldn't parse yaml")
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,14 @@
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_yaml;
+use checklist;
 
-use std::collections::BTreeMap;
-use std::error::Error;
-use std::fs::File;
 use std::path::Path;
-
-use dialoguer::Confirmation;
-
-#[derive(Debug, Serialize, Deserialize)]
-struct CheckListList(BTreeMap<String, CheckList>);
-
-#[derive(Debug, Serialize, Deserialize)]
-struct CheckList(Vec<String>);
-
-fn read_checklists(path: &Path) -> CheckListList {
-    let display = path.display();
-    let file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why.description()),
-        Ok(file) => file,
-    };
-    match serde_yaml::from_reader::<_, CheckListList>(file) {
-        Err(why) => panic!("couldn't read {}: {}", display, why.description()),
-        Ok(s) => s,
-    }
-}
-
-fn ask_question(prompt: &str) -> bool {
-    Confirmation::new()
-        .with_text(&prompt)
-        .interact()
-        .expect("Could not prompt, bailing!")
-}
-
-fn ask_formatted_question(prefix: &str, prompt: &str) -> bool {
-    ask_question(&format!("{}{}?", prefix, prompt))
-}
+use std::process;
 
 fn main() {
     let path = Path::new(".checklist.yml");
-    let checklists = read_checklists(&path);
-    if let Some(checklist) = checklists.0.get("committing") {
-        for item in &checklist.0 {
-            if ask_formatted_question(&"Have you: ", &item) {
-                println!("Great! Continuing...")
-            } else {
-                println!("nevermind then :(");
-            }
-        }
+
+    if let Err(e) = checklist::run(&path) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
     }
 }
